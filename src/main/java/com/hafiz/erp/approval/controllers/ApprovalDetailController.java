84b00ccb859ce6eed809ad87621acb3457e2b0f5
Hafiz Ahmed <rhafizubd@gmail.com>
2021-01-02 09:40:25 +0600

    Refactored CRUD controller and CRUD service

--- src/main/java/com/hafiz/erp/approval/controllers/ApprovalDetailController.java
+++ src/main/java/com/hafiz/erp/approval/controllers/ApprovalDetailController.java
@@ -3,18 +3,15 @@ package com.hafiz.erp.approval.controllers;
 import com.hafiz.erp.approval.core.constants.EndPoint;
 import com.hafiz.erp.approval.core.crud.CrudController;
 import com.hafiz.erp.approval.dataclass.*;
-import com.hafiz.erp.approval.entities.Approval;
 import com.hafiz.erp.approval.entities.ApprovalDetail;
 import com.hafiz.erp.approval.services.ApprovalDetailService;
-import com.hafiz.erp.approval.services.ApprovalService;
-import com.hafiz.erp.approval.utility.ApprovalConversionUtility;
 import com.hafiz.erp.approval.utility.ApprovalDetailConversionUtility;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(EndPoint.APPROVAL_DETAIL)
-public class ApprovalDetailController extends CrudController<ApprovalDetail, ApprovalDetailResponseDTO, ApprovalDetailCreateDTO, ApprovalDetailUpdateDTO> {
+public class ApprovalDetailController extends CrudController<ApprovalDetail, ApprovalDetailDTO> {
 
     ApprovalDetailController(ApprovalDetailService service, ApprovalDetailConversionUtility conversionUtility) {
         super(service, conversionUtility);
--- src/main/java/com/hafiz/erp/approval/dataclass/ApprovalDetailDTO.java
+++ src/main/java/com/hafiz/erp/approval/dataclass/ApprovalDetailDTO.java
@@ -1,12 +1,13 @@
 package com.hafiz.erp.approval.dataclass;
 
+import com.hafiz.erp.approval.core.crud.IdHolder;
 import lombok.Data;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
 @Data
-public class ApprovalDetailCreateDTO {
+public class ApprovalDetailDTO extends IdHolder {
     private UUID approvalId;
     private UUID detailId;
     private String detail;
